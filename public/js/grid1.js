4a191848dad71e9b8884b409a29c6cb1d806a179
AviFoxi <frosenox@gmail.com>
2014-08-06 19:32:36 -0400

    add step wise check box functionality with variable bpm

--- public/js/grid1.js
+++ public/js/grid1.js
@@ -1,3 +1,12 @@
+var sampleUrls = [ 
+	"/samples/Kit/CyCdh_K3Kick-01.wav", 
+	"/samples/Kit/CyCdh_K3Snr-01.wav",
+	"/samples/Kit/CyCdh_K3ClHat-03.wav",
+	"/samples/Kit/CyCdh_K3Crash-07.wav"
+	]
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
 
 	console.log("we're listening");
@@ -6,14 +15,22 @@ document.addEventListener('DOMContentLoaded', function(){
 	$('#machine1').submit(function( event ) {
 	  event.preventDefault();
 	  var bpm = 60 / ( $('#bpm')[0].value );
-	  console.log( bpm  );
+	  var kicksteps = $('.kickstep');
+	  var snaresteps = $('.snarestep');
 
-	  var steps = $('.step');
-	  console.log(steps);
+	  console.log(kicksteps);
+	  console.log(snaresteps);
 
-	  for (var i=0; i < steps.length; i++){
-	  	console.log(steps[i].checked)
-	  }
+	  for (var i=0; i < kicksteps.length; i++){
+	  	var beat = i + 1;
+	  	var time = context.currentTime + (beat * bpm);
+
+	  	if(kicksteps[i].checked){
+	  		loadSound(0, sampleUrls, time);
+	  	} else if (snaresteps[i].checked) {
+	  		loadSound(1, sampleUrls, time);
+	  	}
+	  };
 
 
 	  // rockNroll( (60/bpm), sampleUrls )
