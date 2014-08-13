b721154638fdae3868dfd7b320cb3c9abfe00b05
AviFoxi <frosenox@gmail.com>
2014-08-13 12:57:22 -0400

    define controller functions to handle each piece of AJAX request and direct response data to the audio context buffer

--- public/js/ajaxOnLoad.js
+++ public/js/ajaxOnLoad.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	var controller = new Controller(new AudioContext(), 'view tbd');
 	controller.loadBuffer("/samples/Kit/CyCdh_K3Crash-07.wav");
-
-
 });
 
 var Controller = function(context, view) {
@@ -12,7 +10,34 @@ var Controller = function(context, view) {
 }
 
 Controller.prototype.loadBuffer = function(url){
-	var request = new XMLHttpRequest();
-  request.open("GET", url, true); // Path to Audio File
+	var request = this.prepRequest(url);
+	this.directTraffic2Buffer(request)
+	request.send();
 	console.log(request);
 }
+
+Controller.prototype.prepRequest = function(url) {
+	var request = new XMLHttpRequest();
+  request.responseType = "arraybuffer";
+  request.open("GET", url, true); // Path to Audio File
+  return request;
+}
+
+Controller.prototype.directTraffic2Buffer = function(request){
+	var context = this.context;
+
+	request.onload = function() {
+  	context.decodeAudioData(request.response, function(sample) {
+    	context.buffer = sample;
+	  }, function(){ alert('error loading');});
+	}
+}
+
+
+
+ //  request.onload = function() {
+ //  	context.decodeAudioData(request.response, function(theBuffer) {
+ //    	buffer = theBuffer;
+ //    	playSound(buffer, when);
+	//   }, whoops);
+	// }
